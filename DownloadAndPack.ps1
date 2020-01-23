02ae9769f24c47bd8b937da14104081e909cafa8
chemputer <46601333+chemputer@users.noreply.github.com>
2020-01-23 16:24:48 -0500

    Added pwsh script to download and pack a new version, and fixed error in nuspec

---
+++ DownloadAndPack.ps1
@@ -0,0 +1,19 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$repo = "martinpaljak/GlobalPlatformPro"
+$file = "gp.exe"
+
+$releases = "https://api.github.com/repos/$repo/releases"
+
+Write-Host Determining latest release
+$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+echo $PSScriptRoot
+$download = "https://github.com/$repo/releases/download/$tag/$file"
+echo $download
+echo $tag
+$version = $tag.substring(1)
+
+
+Write-Host Dowloading latest release
+Invoke-WebRequest $download -Out $PSScriptRoot\tools\$file
+ls $PSScriptRoot\tools\
+choco pack --version $version configuration=release --outputdirectory build
